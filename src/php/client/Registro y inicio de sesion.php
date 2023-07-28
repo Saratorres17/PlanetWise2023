505c5f561355ada79c9373c843cd9be1780c5ec3
JSTEx <105887162+JSTEx@users.noreply.github.com>
2023-07-28 08:13:13 -0600

    de ingles a español

--- src/php/client/Registro y inicio de sesion.php
+++ src/php/client/Registro y inicio de sesion.php
@@ -25,7 +25,7 @@ if (!empty($_POST["loginsubmit"])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
