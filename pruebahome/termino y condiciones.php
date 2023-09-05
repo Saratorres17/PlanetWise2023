1e27a9f57177e4f4f263f12dddbac6262ca7bd0e
Tommy <christophernunez641@gmail.com>
2023-09-04 21:23:46 -0600

    Términos y condiciones con css mejorado

--- pruebahome/termino y condiciones.php
+++ pruebahome/termino y condiciones.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Página con Caja Centrada</title>
 </head>
 <body>
-    <div class="bg-white dark:bg-gray-900">
-        <div class="flex justify-center h-screen ">
-            <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://services.meteored.com/img/article/rios-que-desaparecen-en-un-abrir-y-cerrar-de-ojos-magia-glaciar-333021-1_1280.jpeg)">
+    <div class="h-screen">
+        <div class="flex justify-center">
+            <div class="hidden bg-cover lg:block lg:w-2/3 h-[900px]" style="background-image: url(https://services.meteored.com/img/article/rios-que-desaparecen-en-un-abrir-y-cerrar-de-ojos-magia-glaciar-333021-1_1280.jpeg)">
                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40 ">
                     <div>
                         <h2 class="text-5xl font-bold text-white">Tú actitud es importante para nosotros...</h2>
@@ -19,27 +19,25 @@
                 </div>
             </div>
             
-            <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6 mt-16">
+            <div class="flex items-center w-full max-w-md px-6 lg:w-2/6">
                 <div class="flex-1">
                     <div class="text-center">
-                        <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Terminos y condiciones</h2>
+                        <h2 class="text-4xl font-bold text-center">Terminos y condiciones</h2>
                     </div>
-
                     <div class="mt-8">
-                        
                        <li>Este blog proporciona información sobre temas relacionados con el medio ambiente, sostenibilidad y conservación. La información se ofrece con fines informativos</li><br>
                        <li>El contenido publicado en este blog es propiedad del sitio o de sus colaboradores y está protegido por derechos de autor. Puedes compartir y enlazar el contenido, pero no puedes utilizarlo con fines comerciales sin nuestro consentimiento</li><br>
                        <li>Las publicaciones son bienvenidas, pero deben ser respetuosos y relevantes. Nos reservamos el derecho de eliminar publicaciones ofensivos o inapropiados.</li><br>
                        <li>Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento. Los cambios se publicarán en esta página. El uso continuado del sitio después de los cambios constituye tu aceptación de los nuevos términos.</li><br>
                        <li>Al utilizar este sitio, aceptas estos términos y condiciones en su totalidad. Si no estás de acuerdo, por favor, no uses el sitio.</li><br>
-                    </div>
-                    <div class=" flex justify-center">
+
                     <button type="button"class="border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline  ">Aceptar</button>
 
                     <a href="/pruebahome/index.php"><button type="button"class="border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline">Regresar</button></div></a>
                 </div>
+                </div>
             </div>
         </div>
     </div>
-</body>
+    </body>
 </html>
